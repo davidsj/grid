e480d230408882c07b0e62258623a978cb07d5a2
David Schneider-Joseph <david@davidsj.com>
2017-03-28 01:58:41 -0700

    initial board class

--- exhaust.cc
+++ exhaust.cc
@@ -1,9 +1,12 @@
+#include <cstdio>
 #include <iostream>
 #include <cmath>
 
 using namespace std;
 
-int nth_fib(int n, int x, int y) {
+typedef unsigned int uint;
+
+uint nth_fib(uint n, uint x, uint y) {
   float sqrt5 = sqrt(5.);
   float phi = (1. + sqrt5)/2.;
   float psi = (1. - sqrt5)/2.;
@@ -14,9 +17,32 @@ int nth_fib(int n, int x, int y) {
   return round(a*phi + b*psi);
 }
 
-int main(int argc, char **argv) {
-  for (int i = 0; i < 20; i++) {
-    cout << nth_fib(i, 1, 0) << ' ';
+template <uint N>
+class board {
+public:
+  board() {
+    for (int i = 0; i < N; i++) {
+      for (int j = 0; j < N; j++) {
+	vals[i][j] = (i == j);
+      }
+    }
   }
-  cout << endl;
+
+  void print() {
+    for (int i = 0; i < N; i++) {
+      for (int j = 0; j < N; j++) {
+	if (vals[i][j] == 0) printf("   _");
+	else printf("%4d", vals[i][j]);
+      }
+      printf("\n");
+    }
+    printf("\n");
+  }
+
+  uint vals[N][N];
+};
+
+int main(int argc, char **argv) {
+  board<5> b;
+  b.print();
 }
